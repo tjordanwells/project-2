ef9c7d650fbaac7091014fe7f47b2c4ae25c6b73
Claire Stolp <clairemstolp@gmail.com>
2018-08-08 17:25:22 -0400

    fixed linter errors

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -4,6 +4,7 @@ module.exports = function(app) {
   // Load index page
   app.get("/", function(req, res) {
     db.Example.findAll({}).then(function(dbExamples) {
+      console.log(dbExamples);
       res.render("index");
     });
   });
@@ -16,7 +17,9 @@ module.exports = function(app) {
 
   // Load example page and pass in an example by id
   app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
+    db.Example.findOne({ where: { id: req.params.id } }).then(function(
+      dbExample
+    ) {
       res.render("example", {
         example: dbExample
       });
