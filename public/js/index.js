05cec7c3143e42259281f510643f8f9b5a7af1c7
Claire Stolp <clairemstolp@gmail.com>
2018-08-08 10:42:26 -0400

    created login modal to login and signup

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,65 @@
-var $login = $("#login");
-
-$login.on("click", function() {
-  $.ajax("/user", { type: "GET" });
+$("#userAuth").on("click", function() {
+  $(".modal").addClass("is-active");
 });
+
+$("#login").on("click", function() {
+  $("#signup").removeClass("is-info is-active");
+  $("#login").addClass("is-info is-active");
+
+  $("#forms").empty();
+
+  var emailInput = textInput("Email", "userEmail", "your.email@mail.com");
+  $("#forms").append(emailInput);
+
+  var passwordInput = textInput("Password", "userPassword", "Enter your password");
+  $("#forms").append(passwordInput);
+});
+
+$("#signup").on("click", function() {
+  $("#login").removeClass("is-info is-active");
+  $("#signup").addClass("is-info is-active");
+
+  $("#forms").empty();
+  var message = $("<p>").text("Enter your email to register");
+  $("#forms").append(message);
+
+  var emailInput = textInput("Email", "userEmail", "your.email@mail.com");
+  $("#forms").append(emailInput);
+
+  var passwordInput = textInput("Password", "userPassword", "Enter your password");
+  $("#forms").append(passwordInput);
+
+  var confirmPasswordInput = textInput("Re-Enter Password", "confirmPassword", "Enter your password");
+  $("#forms").append(confirmPasswordInput);
+
+  $("#submit").text("Sign up!");
+});
+
+$("#close").on("click", function() {
+  $(".modal").removeClass("is-active");
+});
+
+$("#cancel").on("click", function() {
+  $(".modal").removeClass("is-active");
+});
+
+// Creates a label and text input
+function textInput(labelText, id, placeholder) {
+  var field = $("<div>").addClass("field");
+  var label = $("<label>")
+    .addClass("label")
+    .text(labelText);
+
+  var control = $("<div>").addClass("control");
+
+  var input = $("<input>")
+    .addClass("input")
+    .attr("type", "text")
+    .attr("id", id)
+    .attr("placeholder", placeholder);
+
+  control.append(input);
+  field.append(label);
+  field.append(control);
+  return field;
+}
