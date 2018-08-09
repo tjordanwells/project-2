5cfac507a439c867a92549f7393255dcd171d3e4
Mick Burgess <mburgess@reliaslearning.com>
2018-08-08 22:20:27 -0400

    add validation to user model

--- models/user.js
+++ models/user.js
@@ -1,8 +1,28 @@
 module.exports = function(sequelize, DataTypes) {
   var User = sequelize.define("User", {
-    email: DataTypes.STRING,
-    password: DataTypes.STRING,
-    name: DataTypes.STRING
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        len: [1, 255],
+        isEmail: true
+      }
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1, 255]
+      }
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1, 255]
+      }
+    }
   });
 
   return User;
