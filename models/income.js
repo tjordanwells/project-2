61944fb8aa00d11504d0136ff85550e264d3f5b3
Jasmine Fitts <jasminefitts@Jasmines-MacBook-Pro.local>
2018-08-09 20:29:30 -0400

    updated incometable

--- models/income.js
+++ models/income.js
@@ -1,13 +1,9 @@
-module.exports = (sequelize, DataTypes) => {
-    var Income = sequelize.define("Income", {
-    income_source: {type: DataTypes.STRING},
-    plan: {type: DataTypes.INTEGER},
-    spent: {type:DataTypes.INTEGER},
-    created: {type:DataTypes.STRING},
-    modified: {type:DataTypes.STRING},
-    dateid: {type: DataTypes.DATE}
-    });
-    
-    return Income;
-    };
-    
+module.exports = function(sequelize, DataTypes) {
+  var Income = sequelize.define("Income", {
+    incomeSource: { type: DataTypes.STRING },
+    plan: { type: DataTypes.INTEGER },
+    spent: { type: DataTypes.INTEGER }
+  });
+
+  return Income;
+};
