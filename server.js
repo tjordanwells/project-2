2cfad4fabf9fff804802b56c8cd04d3a0f6fc150
Jasmine Fitts <jasminefitts@Jasmines-MacBook-Pro.local>
2018-08-11 13:23:19 -0400

    added spent routes

--- server.js
+++ server.js
@@ -25,6 +25,7 @@ app.set("view engine", "handlebars");
 // Routes
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
+require("./routes/spentRoutes")(app);
 
 var syncOptions = { force: false };
 
