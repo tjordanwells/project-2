196de2032123250e51c867c956cfd25f02e08e28
Jasmine Fitts <jasminefitts@Jasmines-MacBook-Pro.local>
2018-08-11 22:44:18 -0400

    updated spentRoutes

--- routes/spentRoutes.js
+++ routes/spentRoutes.js
@@ -1,40 +1,38 @@
 var db = require("../models");
 
-
 module.exports = function(app) {
-    app.get("/api/users/:userid/spent", function(req, res) {
-        db.Spent.findAll({
-            where: {
-                id: req.params.userid
-            }
-        }).then(function(dbSpent){
-            res.send(dbSpent)
-        });
+  app.get("/api/users/spent/:userid/", function(req, res) {
+    db.Spent.findAll({
+      where: {
+        id: req.params.userid
+      }
+    }).then(function(dbSpent) {
+      res.send(dbSpent);
     });
+  });
 
-    app.post("/api/spent", function(req, res) {
-        db.Spent.create(req.body).then(function(dbSpent) {
-            res.json(dbSpent) ;
-        });
+  app.post("/api/spent", function(req, res) {
+    db.Spent.create(req.body).then(function(dbSpent) {
+      res.json(dbSpent);
     });
-    app.delete("api/spent/:id", function(req, res) {
-        db.Spent.destroy({
-            where: {
-                id: req.params.id
-            }
-        }).then(function(dbSpent) {
-            res.json(dbSpent);
-        });
+  });
+  app.delete("api/spent/:id", function(req, res) {
+    db.Spent.destroy({
+      where: {
+        id: req.params.id
+      }
+    }).then(function(dbSpent) {
+      res.json(dbSpent);
     });
+  });
 
-    app.put("api/spent", function(req, res) {
-        db.Spent.update(
-            req.body, {
-                where: {
-                    id: req.body.id
-                }
-            }).then(function(dbSpent) {
-                res.json(dbSpent);
-            });
+  app.put("api/spent", function(req, res) {
+    db.Spent.update(req.body, {
+      where: {
+        id: req.body.id
+      }
+    }).then(function(dbSpent) {
+      res.json(dbSpent);
     });
+  });
 };
