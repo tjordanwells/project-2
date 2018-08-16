8620d622b47fbfe03c52b4a65077275e478f8880
Claire Stolp <clairemstolp@gmail.com>
2018-08-16 16:03:32 -0400

    fixed user signin modal problem

--- public/js/index.js
+++ public/js/index.js
@@ -30,7 +30,7 @@ $("#login").on("click", function() {
     "Enter your password"
   );
 
-  var submit = submitBtn("Sign In", "signupSubmit");
+  var submit = submitBtn("Sign In", "signinSubmit");
 
   form
     .append(emailInput)
@@ -80,7 +80,7 @@ $("#signup").on("click", function() {
   );
   form.append(passwordInput);
 
-  var submit = submitBtn("Sign Up", "signinSubmit");
+  var submit = submitBtn("Sign Up", "signupSubmit");
 
   form.append(submit);
 
