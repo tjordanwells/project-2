9775db61dfa9dee02b7be4cf33ba9c146ec5a622
Claire Stolp <clairemstolp@gmail.com>
2018-08-18 01:13:36 -0400

    cleaned up dead code

--- public/js/entries.js
+++ public/js/entries.js
@@ -20,124 +20,12 @@ $("#add").on("click", function() {
   // POST route goes here
   $.post("/user/planned", newEntry).then(function(data) {
     console.log(data);
-    myNewRow(data);
-    //newRow(data);
-    
-    
+    newRow(data);
   });
 });
 
+
 var newRow = function(data) {
-  var newRow = $("<tr>")
-    .addClass("entryRow")
-    .attr("id", "entry-" + " " + data.id)
-    .attr("data-category", data.CategoryId);
-    
-  var newTdOne = $("<td>");
-  
-  var newTdTwo = $("<td>");
-  
-  var newTdThree = $("<td>");
-  
-  var newTdFour = $("<td>");
-  
-  var entryText = $("<div>").addClass("entryText");
-  
-  var header = $("<h3>")
-    .addClass("entry m-xlg-left is-size-7-mobile")
-    .attr("data-entry", data.id)
-    .text(data.entry);
-  
-    var entryEdit = $("<div>").addClass("entryInput hidden");
-  
-  var entryInput = $("<input>")
-    .attr("type", "text")
-    .addClass("input")
-    .attr("name", "editEntry")
-    .attr("id", "editEntry-" + data.id)
-    .attr("value", data.entry);
-  
-    var amountText = $("<div>").addClass("amountText");
-  
-  var amountSpan = $("<span>")
-    .addClass("amount m-sm-left is-size-7-mobile")
-    .attr("id", "amount-" + data.id)
-    .text(data.amount);
-  
-    var amountInput = $("<div>").addClass("amountInput hidden");
-  
-  var amountEdit = $("<input>")
-    .attr("type", "text")
-    .addClass("input")
-    .attr("name", "editInput")
-    .attr("id", "editInput-" + data.id)
-    .attr("value", data.amount);
-
-  var saveDiv = $("<div>").addClass("saveBtn hidden");
-
-  var saveButton = $("<button>")
-    .addClass("button is-primary")
-    .attr("data", "planned");
-
-  var icon = $("<i>").addClass("far fa-save");
-  var dropdown = $("<div>").addClass("dropdown");
-  var trigger = $("<div>").addClass("dropdown-trigger");
-  var controlSpan = $("<span>")
-    .attr("aria-haspopup", true)
-    .attr("aria-controls", data.id + "-menu");
-
-  var ellipsis = $("<i>").addClass("icon fas fa-ellipsis-h");
-
-  var menu = $("<div>")
-    .addClass("dropdown-menu")
-    .attr("id", data.id + "-menu")
-    .attr("role", "menu");
-
-  var dropdownContent = $("<div>").addClass("dropdown-content p-sm");
-
-  var infoBtn = $("<button>")
-    .addClass("button is-info is-fullwidth m-xsm-bottom")
-    .attr("data-section", "planned")
-    .attr("data-action", "edit")
-    .attr("id", data.id + "_edit")
-    .text("Edit");
-
-  var deleteBtn = $("<button>")
-    .addClass("button is-danger is-fullwidth")
-    .attr("data-section", "planned")
-    .attr("data-action", "delete")
-    .attr("id", data.id + "_delete")
-    .text("Delete");
-
-    console.log($(".entries[data-category=" + data.CategoryId + "']"));
-  $(".entries[data-category=" + data.CategoryId + "']")
-    .append(newRow)
-    .appendChild(newTdOne);
-  newTdOne.appendChild(entryText);
-  entryText.appendChild(header);
-  entryText.append(entryInput);
-  entryInput.appendChild(entryEdit);
-  newTdOne.append(newTdTwo);
-  newTdTwo.appendChild(amountText);
-  amountText.appendChild(amountSpan);
-  amountText.append(amountInput);
-  amountInput.appendChild(amountEdit);
-  newTdTwo.append(newTdThree);
-  newTdThree.appendChild(saveDiv);
-  saveDiv.appendChild(saveButton);
-  saveButton.appendChild(icon);
-  saveDiv.append(dropdown);
-  dropdown.appendChild(trigger);
-  trigger.appendChild(controlSpan);
-  controlSpan.appendChild(ellipsis);
-  dropdown.append(menu);
-  menu.appendChild(dropdownContent);
-  dropdownContent.appendChild(infoBtn);
-  infoBtn.append(deleteBtn);
-  newTdThree.append(newTdFour);
-}
-
-var myNewRow = function(data) {
   //console.log(data);
   //-----Table Row-------//
   var row = $("<tr>")
