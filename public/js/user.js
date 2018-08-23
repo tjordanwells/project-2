4c6fe3aa85f8d0b73b36f75bcb77056701c3c3c7
Claire Stolp <clairemstolp@gmail.com>
2018-08-23 18:27:48 -0400

    fixed media queries

--- public/js/user.js
+++ public/js/user.js
@@ -53,44 +53,40 @@ var dropdownlistener = function() {
   });
 };
 
+//listens for dropdown buttons 
 var dropdownContentListener = function() {
   $(".dropdown-content > button").on("click", function() {
     var $action = $(this).attr("data-action");
-    console.log($action);
+
     var idData = $(this)
       .attr("id")
       .split("_");
 
-    console.log(idData);
+
     $(".dropdown").removeClass("is-active");
   
     var isCat = idData[0].split("-")[0];
-    var catId = idData[0].split("-")[1]
-    id = idData[1].split('-')[1];
-    console.log(catId);
+    
+    var id;
     if ($action === "edit") {
-      if(isCat ==="cat"){
-        console.log("I work!");
+      if(isCat ==="cat"){   
+        var catId = idData[0].split("-")[1];
         categoryEditToggleOn(catId);
       }else{
+        id = idData[0];
         toggleInputOn("entry-" + id);
       }
-      
     } else if ($action === "move") {
+      id = idData[0];
       $(".modal").addClass("is-active");
-      moveModal(id);
+      moveModal("entry-" + id);
     } else if ($action === "delete") {
+      id = idData[0];
       $(".modal").addClass("is-active");
-      deleteModal(id);
+      deleteModal("entry-" + id);
     }
 
-    saveListener(id);
-  });
-};
-
-var saveListener = function(id) {
-  $(".saveBtn").on("click", function() {
-    toggleInputOff(id);
+    inputSaveListener(id);
   });
 };
 
@@ -117,7 +113,6 @@ var toggleInputOn = function(id) {
 var toggleInputOff = function(id) {
   var $entryText = $("#" + id + " td:nth-child(1) > .entryText");
   var $entryInput = $("#" + id + " td:nth-child(1) > .entryInput");
-
   var $amountText = $("#" + id + " td:nth-child(2) > .amountText");
   var $amountInput = $("#" + id + " td:nth-child(2) > .amountInput");
 
@@ -138,13 +133,16 @@ var moveModal = function(entryId) {
 
   $body.empty();
 
-  var entryCat = entryId.split("-")[0];
-  entryCat = entryCat[0].toUpperCase() + entryCat.slice(1);
-  var entry = $("#" + entryId + " td:nth-child(1) h3").text();
+  var catId = $("#" + entryId).attr("data-category");
+  var catSelector = ".category[data-category=" + catId +"]"
+  var cat = $(catSelector).text();
+
+  var entry = $("#" + entryId).find(".entry").text();
+
   $title.html("<p class='modal-card-title'>Move Entry</p>");
 
   var $message = $("<p>").addClass("has-text-weight-bold");
-  $message.text(`Move \"${entry}\" from \"${entryCat}\" to: `);
+  $message.text(`Move \"${entry}\" from \"${cat}\" to: `);
 
 
   var $selectDiv = $("<div>").addClass("select m-md");
@@ -162,15 +160,20 @@ var moveModal = function(entryId) {
 
   $selectDiv.append($select);
   $body.append($message).append($selectDiv);
+
   $(".modal-card-foot :nth-child(1)")
     .removeClass("is-danger")
     .addClass("is-success")
+    .attr("id", "moveConfirm")
     .text("Save Changes");
   
   $(".modal-card-foot :nth-child(2)")
     .removeClass("is-danger")
     .addClass("is-info");
+
+  moveListener("#moveConfirm");
 };
+
 
 var deleteModal = function (entryId) {
   var $title = $(".modal-card-title");
@@ -184,24 +187,74 @@ var deleteModal = function (entryId) {
   $title.html("<p class='modal-card-title'>Are You Sure?</p>");
   var $message = $("<p>")
     .addClass("has-text-weight-bold")
-    .text(`Are you sure you wish to delete ${entryId}?`);
+    .text(`Are you sure you wish to delete \"${entry}\"?`);
 
   $body.append($message);
   $(".modal-card-foot :nth-child(1)")
     .removeClass("is-success")
     .addClass("is-danger")
+    .attr("id", "deleteConfirm")
     .text("Delete");
   
   $(".modal-card-foot :nth-child(2)")
     .removeClass("is-danger")
     .addClass("is-info");
+  
+  deleteListener("#deleteConfirm");
 }
+
+
 
 //toggle category edit
 var categoryEditToggleOn = function(id) {
-  id = "#catInput-" + id;
-  console.log($(id))
- 
+  var catSelector = $("#category-" + id);
+  console.log(catSelector.find(".saveBtn"))
+  var inputElem = $("#catInput-" + id);
+  $(".catText[data-category=" + id +"]").addClass("hidden");
+  $(catSelector).find(".saveBtn").removeClass("hidden");
+  $(catSelector).find(".dropdown-trigger").addClass("hidden");
+  inputElem.removeClass("hidden");
+  catSaveListener(id);
+}
+
+var deleteListener = function(selector){
+  $(selector).on("click", function() {
+    //DELETE request here!
+
+    $(".modal").removeClass("is-active");
+  });
+}
+
+var moveListener = function(selector){
+  $(selector).on("click", function() {
+    //PUT request here!
+
+    $(".modal").removeClass("is-active");
+  });
+}
+
+var inputSaveListener = function(id) {
+  $(".saveBtn").on("click", function() {
+    //PUT route goes here
+
+    toggleInputOff("entry-" + id);
+  });
+};
+
+var catSaveListener = function(id) {
+  var selector = $("#category-" + id);
+
+  selector.find(".saveBtn").on("click", function() {
+  
+    $(this).addClass("hidden");
+    selector.find(".dropdown-trigger").removeClass("hidden");
+    $("#catInput-" + id).addClass("hidden");
+    $(".catText[data-category=" + id + "]")
+      .removeClass("hidden");
+  //PUT request here
+
+  });
+
 }
 
 dropdownlistener();
